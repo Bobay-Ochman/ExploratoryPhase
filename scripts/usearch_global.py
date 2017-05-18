57e930e738f34cc46d77696d36138c5c72cfc086
Brian <brian.e2014@gmail.com>
2017-05-18 02:41:29 -0500

    corrected comand

--- scripts/usearch_global.py
+++ scripts/usearch_global.py
@@ -11,6 +11,6 @@ for hmm in markers:
 	if hmm.endswith('.txt'):
 		nb+=1
 		print hmm,' ',nb ,'/44'
-		command = PATH_TO_USEARCH + ' -global -query '+PATH_TO_ME+'/uploads/'+timeStamp+'/'+testGenome+'   -db  '+PATH_TO_ME+'/markers/' + hmm + '  -id 0.7 -blast6out '+PATH_TO_ME+'/uploads/'+timeStamp+'/'+testGenome+'_' + hmm + '.out'
+		command = PATH_TO_USEARCH + ' -usearch_global -query '+PATH_TO_ME+'/uploads/'+timeStamp+'/'+testGenome+'   -db  '+PATH_TO_ME+'/markers/' + hmm + '  -id 0.7 -blast6out '+PATH_TO_ME+'/uploads/'+timeStamp+'/'+testGenome+'_' + hmm + '.out'
 		print command
 		os.system(command)
