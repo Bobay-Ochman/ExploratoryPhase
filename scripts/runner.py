effebd427dca4f1588d53c9befcfb1d72585a0d2
Brian <brian.e2014@gmail.com>
2018-03-20 10:12:23 -0500

    corrected sending two emails

--- scripts/runner.py
+++ scripts/runner.py
@@ -17,14 +17,14 @@ remArgs = ' '+sys.argv[1]+' '+sys.argv[2]+' '+sys.argv[3]+' '
 print "--- cleaning the names of the genes"
 os.system('python '+PATH_TO_ME + 'scripts/' + 'clean_gene_names.py '+remArgs + ' &> '+PATH_TO_UPLOAD+'out/01_clean.txt')
 
-try:
+if os.path.isfile(PATH_TO_UPLOAD+'cleaning_errors.txt'):
 	f = open(PATH_TO_UPLOAD+'cleaning_errors.txt',"r")
 	emailMessage = 'The file you have uploaded appears to not be in the proper format. Please be sure to upload a multi-FASTA file of the protein-coding genes of the genome. An error log is listed below.\n\nLog:\n'
 	emailMessage += "".join(f.readlines())
 	sendEmail(emailMessage)
 	os.system("echo quitting, going home > out/11_stopping.txt")
 	quit()
-except:
+else:
 	sendEmail("We've started your analysis and will email you at this address upon completion.")
 
 print "--- usearch"
